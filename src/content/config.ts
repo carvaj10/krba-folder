dc6472da7862c1062a345a8b2a731227606b1a43
Carvajal luis <joseluiscarvajal19@gmail.com>
2025-05-11 21:29:25 -0500

    Fix: Se estandariza Interfaz

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,17 +11,6 @@ const work = defineCollection({
     companyUrl: z.string()
   })
 });
-const hackathon = defineCollection({
-  type: 'content',
-  schema: z.object({
-    company: z.string(),
-    role: z.string(),
-    dateStart: z.date(),
-    dateEnd: z.date(),
-    location: z.string(),
-    companyUrl: z.string()
-  })
-});
 
 const projects = defineCollection({
   type: 'content',
@@ -39,4 +28,4 @@ const projects = defineCollection({
     })
 });
 
-export const collections = { work, hackathon, projects };
+export const collections = { work, projects };
